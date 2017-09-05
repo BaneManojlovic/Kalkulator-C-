5c074a7ce332e05d882694acbdc9f80b39b53454
BaneManojlovic <manojlobaki@gmail.com>
2017-09-05 15:30:16 +0200

    peti komit

---
+++ baza2.sql
@@ -0,0 +1,20 @@
+/*select*from jwd13_17.tbl_activity;
+select*from jwd13_17.tbl_user; */
+/*
+create table korisnici (
+	Rb int,
+    Ime varchar(50),
+    Prezime varchar(200),
+    Email varchar(200),
+    Lozinka varchar(200)
+
+);
+*/
+
+select*from korisnici;
+/*
+INSERT INTO korisnici (Rb, Ime, Prezime, Email, Lozinka) VALUES (1, 'Petar','Petrovic', 'petar@gmail.com', 'pp001');
+INSERT INTO korisnici (Rb, Ime, Prezime, Email, Lozinka) VALUES (2, 'Marko','Markovic', 'marko@gmail.com', 'mm002');
+INSERT INTO korisnici (Rb, Ime, Prezime, Email, Lozinka) VALUES (3, 'Jovan','Jovanovic', 'jovan@gmail.com', 'jj003');
+INSERT INTO korisnici (Rb, Ime, Prezime, Email, Lozinka) VALUES (4, 'Steva','Stevic', 'steva@gmail.com', 'ss004');
+*/
